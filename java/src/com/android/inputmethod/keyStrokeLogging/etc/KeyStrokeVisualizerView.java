d47bc002ad2ec329f17f5242527f34a8a97a652f
MatKier <MatKier@users.noreply.github.com>
2018-07-13 12:54:35 +0200

    Implemented more intuitive holdtime and pressure visualization

--- java/src/com/android/inputmethod/keyStrokeLogging/etc/KeyStrokeVisualizerView.java
+++ java/src/com/android/inputmethod/keyStrokeLogging/etc/KeyStrokeVisualizerView.java
@@ -20,8 +20,8 @@ public class KeyStrokeVisualizerView extends View {
     // Measured through key.getHitBox().height();
     private static final double standardKeyHitboxHeight = 220.0;
 
-    private static final int KEY_SHAPE_WIDTH = 100;
-    private static final int KEY_SHAPE_HEIGHT = 160;
+    private static final int BASE_KEY_SHAPE_WIDTH = 100;
+    private static final int BASE_KEY_SHAPE_HEIGHT = 160;
 
     private static final int colorBackground = Color.parseColor("#FAFAFA");
     private static final int colorRectangle = Color.parseColor("#D9DDE0");
@@ -29,9 +29,9 @@ public class KeyStrokeVisualizerView extends View {
     private static final Paint circlePaint = new Paint();
     private static final Paint rectanglePaint = new Paint();
     private static final Paint keyLabelPaint = new Paint();
-    private static final Paint keyLabelPaintBold = new Paint();
 
     private static final int LABEL_FONT_SIZE = 85;
+    private static final int LABEL_FONT_SIZE_DROIDSANSMONO = 70;
 
     private static final int CORNER_RADIUS = 15;
 
@@ -39,11 +39,13 @@ public class KeyStrokeVisualizerView extends View {
 
     private static final int MIN_HOLD_TIME_MS = 100;
     private static final int MAX_HOLD_TIME_MS = 500;
-    private static final int HOLD_TIME_SCALE = 15;
+    private static final int HOLD_TIME_SCALE = 4;
 
     private static final int MIN_FLIGHT_TIME_MS = 100;
     private static final int MAX_FLIGHT_TIME_MS = 1000;
     private static final int FLIGHT_TIME_SCALE = 10;
+
+    private static final int PRESSURE_SCALE = 80;
 
 
     private final RectF rectf = new RectF();
@@ -58,20 +60,13 @@ public class KeyStrokeVisualizerView extends View {
         rectanglePaint.setStyle(Paint.Style.FILL);
         rectanglePaint.setAntiAlias(true);
 
-        Typeface tfCourierNew = Typeface.createFromAsset(getContext().getAssets(), "fonts/CourierNew.ttf");
-        Typeface tfCourierNewBold = Typeface.createFromAsset(getContext().getAssets(), "fonts/CourierNewBold.ttf");
+        Typeface tfDroidSansMono = Typeface.createFromAsset(getContext().getAssets(), "fonts/DroidSansMono.ttf");
 
         keyLabelPaint.setColor(Color.BLACK);
-        keyLabelPaint.setTextSize(LABEL_FONT_SIZE);
-        keyLabelPaint.setTypeface(tfCourierNew);
+        keyLabelPaint.setTextSize(LABEL_FONT_SIZE_DROIDSANSMONO);
+        keyLabelPaint.setTypeface(tfDroidSansMono);
         keyLabelPaint.setStyle(Paint.Style.FILL);
         keyLabelPaint.setAntiAlias(true);
-
-        keyLabelPaintBold.setColor(Color.BLACK);
-        keyLabelPaintBold.setTextSize(LABEL_FONT_SIZE);
-        keyLabelPaintBold.setTypeface(tfCourierNewBold);
-        keyLabelPaintBold.setStyle(Paint.Style.FILL);
-        keyLabelPaintBold.setAntiAlias(true);
     }
 
     public void setKeyStrokeList(List<KeyStrokeDataBean> keyStrokeList) {
@@ -87,8 +82,8 @@ public class KeyStrokeVisualizerView extends View {
             return;
         }
 
-        int currentX = 0;
-
+        int yStartingPoint = canvas.getHeight() / 2 - BASE_KEY_SHAPE_HEIGHT / 2;
+        int currentX = 30;
         for (KeyStrokeDataBean bean : keyStrokeList) {
             /* Move to the right (in relation to the flightTime)*/
             if (bean.getEventType().equals(StudyConstants.EVENT_TYPE_DOWN) && bean.getFlightTime() != -1) {
@@ -98,52 +93,53 @@ public class KeyStrokeVisualizerView extends View {
                 } else if (fTime > MAX_FLIGHT_TIME_MS) {
                     currentX = currentX + MAX_FLIGHT_TIME_MS / FLIGHT_TIME_SCALE;
                 } else {
-                    currentX = (int)(currentX + fTime / FLIGHT_TIME_SCALE);
+                    currentX = (int) (currentX + fTime / FLIGHT_TIME_SCALE);
                 }
             } else if (bean.getEventType().equals(StudyConstants.EVENT_TYPE_UP)) {
                 /* Draw key shape */
-                rectf.set(currentX, 0, currentX + KEY_SHAPE_WIDTH, KEY_SHAPE_HEIGHT);
+                int holdTime = (int) bean.getHoldTime();
+                int keyshapeWidth;
+                if (holdTime <= MIN_HOLD_TIME_MS) {
+                    keyshapeWidth = BASE_KEY_SHAPE_WIDTH;
+                } else if (holdTime >= MAX_HOLD_TIME_MS) {
+                    keyshapeWidth = BASE_KEY_SHAPE_WIDTH + (MAX_HOLD_TIME_MS - 100) / HOLD_TIME_SCALE;
+                } else {
+                    keyshapeWidth = BASE_KEY_SHAPE_WIDTH + (holdTime - 100) / HOLD_TIME_SCALE;
+                }
+
+                rectf.set(currentX, yStartingPoint, currentX + keyshapeWidth, BASE_KEY_SHAPE_HEIGHT + yStartingPoint);
                 canvas.drawRoundRect(rectf, CORNER_RADIUS, CORNER_RADIUS, rectanglePaint);
 
                 /* Draw label on key */
                 String label = bean.getKeyValue().length() == 1 ? bean.getKeyValue() : "";
-                Paint p = bean.getPressure() > 0.35f ? keyLabelPaintBold : keyLabelPaint;
-                int halfLabelWidth = (int) (p.measureText(label) / 2);
-                int halfLabelHeight = (int) (p.measureText(label) / 2);
-                canvas.drawText(label, rectf.centerX() - halfLabelWidth, rectf.centerY() + halfLabelHeight, p);
+                int halfLabelWidth = (int) (keyLabelPaint.measureText(label) / 2);
+                int halfLabelHeight = (int) (keyLabelPaint.measureText(label) / 2);
+                canvas.drawText(label, rectf.centerX() - halfLabelWidth, rectf.centerY() + halfLabelHeight, keyLabelPaint);
 
-
-                /* Calculate Circle radius */
-                long hTime = bean.getHoldTime();
-                float radius = 0;
-                if (hTime < MIN_HOLD_TIME_MS) {
-                    radius = MIN_HOLD_TIME_MS / HOLD_TIME_SCALE;
-                } else if (hTime > MAX_HOLD_TIME_MS) {
-                    radius = MAX_HOLD_TIME_MS / HOLD_TIME_SCALE;
-                } else {
-                    radius = bean.getHoldTime() / HOLD_TIME_SCALE;
-                }
+                /* Calculate Circle radius in dependence of the pressure*/
+                float pressure = bean.getPressure();
+                float radius = pressure * PRESSURE_SCALE;
 
                 /* Set Circle Alpha in dependence of the radius */
                 if (radius <= 10f) {
                     circlePaint.setAlpha(255);
                 } else {
-                    circlePaint.setAlpha((int)(170 - radius));
+                    circlePaint.setAlpha((int) (170 - radius));
                 }
 
                 /* Draw offset circle on key */
                 double offsetXFactor = (bean.getOffsetX() / standardKeyHitboxWidth);
                 // Make sure the circle can't go out of bounds (e.g. Space bar)
-                offsetXFactor = (Math.abs(offsetXFactor) > 0.45 ? (0.45 *  Math.signum(offsetXFactor)) : offsetXFactor);
+                offsetXFactor = (Math.abs(offsetXFactor) > 0.45 ? (0.45 * Math.signum(offsetXFactor)) : offsetXFactor);
                 float actualOffsetX = (float) (offsetXFactor * rectf.width());
 
                 double offSetYFactor = (bean.getOffsetY() / standardKeyHitboxHeight);
                 // Make sure the circle can't go out of bounds
-                offSetYFactor = (Math.abs(offSetYFactor) > 0.45 ? (0.45 *  Math.signum(offSetYFactor)) : offSetYFactor);
-                float actualOffsetY = (float)(offSetYFactor * rectf.height());
+                offSetYFactor = (Math.abs(offSetYFactor) > 0.45 ? (0.45 * Math.signum(offSetYFactor)) : offSetYFactor);
+                float actualOffsetY = (float) (offSetYFactor * rectf.height());
                 canvas.drawCircle(rectf.centerX() + actualOffsetX, rectf.centerY() + actualOffsetY, radius, circlePaint);
 
-                currentX = currentX + (int)rectf.width();
+                currentX = currentX + (int) rectf.width();
             }
         }
     }
